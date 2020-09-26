63a95300432b3bab38fc7a19c7db7bbfbdf3dc3e
Tabish Fazli <tfazl051@uottawa.ca>
2020-09-26 16:44:00 -0400

    User class is made

---
+++ app/src/main/java/com/example/a2105projectgroup13/User.java
@@ -0,0 +1,32 @@
+package com.example.a2105projectgroup13;
+
+public class User{
+
+    private String firstName;
+    private String lastName;
+    private String accountType;
+    private int userID;
+
+    public User(String nameNumOne, String nameNumTwo, String acType, int id){
+        this.firstName = nameNumOne;
+        this.lastName = nameNumTwo;
+        this.accountType = acType;
+        this.userID = id;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public String getAccountType(){
+        return accountType;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+}
