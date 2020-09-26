d8127357f0cbba6caa3d2515d8e1e7e42438200b
Tabish Fazli <tfazl051@uottawa.ca>
2020-09-26 16:46:45 -0400

    Admin class is made

---
+++ app/src/main/java/com/example/a2105projectgroup13/Admin.java
@@ -0,0 +1,7 @@
+package com.example.a2105projectgroup13;
+
+public class Admin extends User {
+    public Admin(String nameNumOne, String nameNumTwo, String acType, int id) {
+        super(nameNumOne, nameNumTwo, acType, id);
+    }
+}
