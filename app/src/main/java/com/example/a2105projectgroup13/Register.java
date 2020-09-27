daaa5da0b7ce5912ba239dc1361a147ff902464d
Nicholas Gin <ngin102@uottawa.ca>
2020-09-26 22:59:13 -0400

    Add onRadioButtonClicked method to Register.java, ad radio buttons for customer and branch to xml, update Register.java

--- app/src/main/java/com/example/a2105projectgroup13/Register.java
+++ app/src/main/java/com/example/a2105projectgroup13/Register.java
@@ -3,9 +3,11 @@ package com.example.a2105projectgroup13;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,7 +24,6 @@ public class Register extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
 
 
-    //onCreate is called when
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,13 +35,55 @@ public class Register extends AppCompatActivity {
         editTextUserName = findViewById(R.id.editTextUserName);
         editTextEmailAddress = findViewById(editTextEmailAddress);
         editTextPassword = findViewById(editTextPassword);
+        registerButton = findViewById(registerButton);
 
         //Initialize progress bar
         progressBar = findViewById(R.id.progressBar)
 
         //Initialize firebase authenticator by getting its instance.
         firebaseAuth = FirebaseAuth.getInstance();
+
+        //Validation of all fields where user input is required will be coded for later.
+        //Code what happens when the register button is clicked.
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Assuming validation of these fields has occurred...
+                String firstName = editTextFirstName.getText().toString().trim();
+                String lastName = editTextLastName.getText().toString().trim();
+                String userName = editTextUserName.getText().toString().trim();
+                String emailAddress = editTextEmailAddress.getText().toString().trim();
+                String password = editTextPassword.getText().toString();
+                String accountType = onRadioButtonClicked(View v);
+
+                //Make the progress bar visible to the user.
+                //The progress bar will serve as an indicator to the user that the application is "loading," as it
+                //sends the user's information to Firebase to be stored.
+                progressBar.setVisibility(View.VISIBLE);
+
+                //Now, let's send the user's information to be stored in Firebase.
+                //F
+                firebaseAuth.createUserWithEmailAndPassword(emailAddress, password);
+            }
+
+
+    //Code adapted from official Android Developers website
+    public String onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.customerAccountButton:
+                if (checked)
+                    return "Customer";
+            case R.id.branchButton:
+                if (checked)
+                    return "Branch";
+            }
+        }
     }
+
 
 
 }
