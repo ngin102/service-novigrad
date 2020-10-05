7b1029be5047578d3c9bc03c2dffdb5748d9f94a
journalWheat <jaredaustin.wagner@gmail.com>
2020-10-05 17:05:59 -0400

    Implement constructor for User taking userID. This is called to from the subclasses of account to make accounts.

--- app/src/main/java/com/example/a2105projectgroup13/User.java
+++ app/src/main/java/com/example/a2105projectgroup13/User.java
@@ -1,30 +1,52 @@
 package com.example.a2105projectgroup13;
 
-public class User{
+import android.content.Intent;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import static android.view.View.INVISIBLE;
+
+public class User {
 
     private String firstName;
     private String lastName;
     private String accountType;
+    // Variables used to access Firebase
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
 
-    public User(String firstName, String lastName, String accountType){
+    // constructors
+    public User(String firstName, String lastName, String accountType) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.accountType = accountType;
     }
 
     public User(String userID) {
-        // empty for now
+        // initialize FirebaseAuthorization and FirebaseDatabase
+        firebaseDatabase.getInstance();
+        this.firstName = firebaseDatabase.getReference("Users").child(userID).child("firstName").toString();
+        this.lastName = firebaseDatabase.getReference("Users").child(userID).child("lastName").toString();
+        this.accountType = firebaseDatabase.getReference("Users").child(userID).child("accountType").toString();
     }
 
-    public String getFirstName(){
+// public getters
+
+    public String getFirstName() {
         return firstName;
     }
 
-    public String getLastName(){
+    public String getLastName() {
         return lastName;
     }
 
-    public String getAccountType(){
+    public String getAccountType() {
         return accountType;
     }
 }
