003472d71076fde6e0e2661e34abb9d7a61c55a0
Nicholas Gin <ngin102@uottawa.ca>
2020-10-06 20:34:48 -0400

    Update Register and Login activities to accomodate for JUnit.

---
+++ app/src/test/java/com/example/a2105projectgroup13/LoginTest.java
@@ -0,0 +1,12 @@
+package com.example.a2105projectgroup13;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginTest {
+
+    @Test
+    public void onCreate() {
+    }
+}
