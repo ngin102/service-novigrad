fcd94514523e21d2322d7b8e5a1ad40eb97f8c7e
Nicholas Gin <ngin102@uottawa.ca>
2020-10-06 22:49:21 -0400

    Update login acitivty.

--- app/src/main/java/com/example/a2105projectgroup13/MainActivity.java
+++ app/src/main/java/com/example/a2105projectgroup13/MainActivity.java
@@ -1,14 +1,81 @@
 package com.example.a2105projectgroup13;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+
+    private User loggedInUser;
+
+    private String firstNameString;
+    private String lastNameString;
+    private String accountTypeString;
+    private String uid;
+
+    private ValueEventListener listener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getUid();
+        DatabaseReference firstName = firebaseDatabase.getReference("Users").child(uid).child("firstName");
+        DatabaseReference accountType = firebaseDatabase.getReference("Users").child(uid).child("accountType");
+
+        firstName.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot){
+                firstNameString = dataSnapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(MainActivity.this, "Error getting account type. Relaunch app.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        });
+
+        accountType.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot){
+                accountTypeString = dataSnapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(MainActivity.this, "Error getting account type. Relaunch app.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        });
+
+
+        TextView welcomeText = (TextView) findViewById(R.id.welcomeText);
+        welcomeText.setText("Welcome "+firstNameString+" You are signed in as: "+accountTypeString);
+    }
+
+
+    private void getUid(){
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = firebaseDatabase.getInstance();
+
+        FirebaseUser user = firebaseAuth.getInstance().getCurrentUser();
+        uid = user.getUid();
     }
 }
