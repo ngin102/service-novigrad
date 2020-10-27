71c2fab7ad13104dd3b3e75ceb36d2e2bed777ab
Nicholas Gin <ngin102@uottawa.ca>
2020-10-27 18:16:33 -0400

    Add ability to store Forms in Firebase from NewForm activity.

--- app/src/main/java/com/example/a2105projectgroup13/Form.java
+++ app/src/main/java/com/example/a2105projectgroup13/Form.java
@@ -1,25 +1,22 @@
 package com.example.a2105projectgroup13;
 
+import java.util.ArrayList;
+
 public class Form {
 
-    private int checkBoxes;
     private String type;
-    private String[] fields;
-    private String formName;
+    private String formName;;
 
-    public Form(int checkBoxes, String type, String formName){
-        this.checkBoxes = checkBoxes;
+    public Form(String type, String formName){
         this.type = type;
-        String[] fields = new String[checkBoxes];
-        this.fields = fields;
         this.formName = formName;
     }
 
-    public int getCheckBoxes(){return checkBoxes;}
+    public String getType (){
+        return type;
+    }
 
-    public String getTypeForm(){return type;}
-
-    public String[] getFieldName(){return fields;}
-
-    public String getFormName(){return formName;}
+    public String getFormName(){
+        return formName;
+    }
 }
--- app/src/main/java/com/example/a2105projectgroup13/NewForm.java
+++ app/src/main/java/com/example/a2105projectgroup13/NewForm.java
@@ -1,5 +1,6 @@
 package com.example.a2105projectgroup13;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,19 +10,36 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class NewForm extends AppCompatActivity {
+    private EditText editTextFormName;
+    private LinearLayout fieldList;
+    private Button addFieldButton;
+    private Button addFormButton;
+    private FirebaseDatabase firebaseDatabase;
 
-    LinearLayout fieldList;
-    Button addFieldButton;
+    private ArrayList<String> fields;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_form);
 
+        editTextFormName = findViewById(R.id.editTextFormName);
         fieldList = findViewById(R.id.fieldList);
         addFieldButton = findViewById(R.id.addFieldButton);
+        addFormButton = findViewById(R.id.addFormButton);
+        fields = new ArrayList<>();
+        firebaseDatabase = firebaseDatabase.getInstance();
 
         addFieldButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,13 +48,18 @@ public class NewForm extends AppCompatActivity {
             }
         });
 
+        addFormButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                submitFormOnClick(view);
+            }
+        });
     }
 
     private void addFieldToList(){
         LayoutInflater inflater = getLayoutInflater();
         final View field = inflater.inflate(R.layout.add_field, null);
 
-        EditText editTextFieldName = (EditText) field.findViewById(R.id.editTextFieldName);
         Button removeFieldButton = (Button) field.findViewById(R.id.removeFieldButton);
 
         removeFieldButton.setOnClickListener(new View.OnClickListener() {
@@ -49,4 +72,59 @@ public class NewForm extends AppCompatActivity {
         fieldList.addView(field);
     }
 
+
+    private boolean validateForEmptyFields(){
+        for (int i = 0; i < fieldList.getChildCount(); i++){
+            View selectedField = fieldList.getChildAt(i);
+            EditText selectedFieldName = (EditText) selectedField.findViewById(R.id.editTextFieldName);
+            String fieldName = selectedFieldName.getText().toString().trim();
+
+            if (fieldName.isEmpty()) {
+                Toast.makeText(NewForm.this, "Please name all fields or remove unnamed fields.", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void submitFormOnClick(View v) {
+        String formName = editTextFormName.getText().toString().trim();
+
+        if (formName.equals("")){
+            Toast.makeText(NewForm.this, "Please enter a name for the Form.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (validateForEmptyFields() == true && (! formName.equals("") )) {
+            fields.clear();
+            for (int i = 0; i < fieldList.getChildCount(); i++) {
+                View selectedField = fieldList.getChildAt(i);
+                EditText selectedFieldName = (EditText) selectedField.findViewById(R.id.editTextFieldName);
+                String fieldName = selectedFieldName.getText().toString().trim();
+
+                fields.add(fieldName);
+            }
+
+            final Form formToAddToService = new Form("form", formName);
+
+            firebaseDatabase.getReference("Services").child("Service Name").child(formName).setValue(formToAddToService).addOnCompleteListener(NewForm.this, new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        DatabaseReference fieldsInFirebase = firebaseDatabase.getReference("Services").child("Service Name").child("fields");
+                        for (int i = 0; i < fields.size(); i++) {
+                            String fieldToAdd = fields.get(i);
+                            fieldsInFirebase.child(Integer.toString(i)).setValue(fieldToAdd);
+                        }
+
+                        Toast.makeText(NewForm.this, "Form added to service", Toast.LENGTH_SHORT).show();
+                    } else {
+                        //If the user's information was not successfully stored in Firebase Database, give the user this message prompt.
+                        Toast.makeText(NewForm.this, "There was a problem adding this Form to your service.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
 }
