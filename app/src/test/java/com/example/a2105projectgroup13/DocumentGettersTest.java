e622c9b22ab3c1a40e2950e2947c662eb887f423
Nicholas Gin <ngin102@uottawa.ca>
2020-11-16 00:33:08 -0500

    Add/update junits for DocumentGetters, FormGetters, RequirementGetters and ServiceGetters.

---
+++ app/src/test/java/com/example/a2105projectgroup13/DocumentGettersTest.java
@@ -0,0 +1,22 @@
+package com.example.a2105projectgroup13;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DocumentGettersTest {
+
+    @Test
+    public void testGetters() {
+        Document testDocument = new Document("document", "Photo ID", "PDF", "This is a document");
+        Service service2 = new Service("a service", "20.00");
+        testDocument.setService(service2);
+
+        assertEquals("document", testDocument.getType());
+        assertEquals("Photo ID", testDocument.getName());
+        assertEquals("PDF", testDocument.getFileType());
+        assertEquals("This is a document", testDocument.getDescription());
+        assertEquals(service2, testDocument.getService());
+    }
+
+}
