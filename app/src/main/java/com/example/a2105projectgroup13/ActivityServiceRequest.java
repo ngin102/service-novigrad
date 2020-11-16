8c4bff07b94951507272b889e26a46bfc70d09e5
Grant Wagner <gwagn012@uottawa.ca>
2020-11-15 22:24:59 -0500

    Implement backButton for ActivityServiceRequest.
    Pressing backButton returns the user to the previous menu.

--- app/src/main/java/com/example/a2105projectgroup13/ActivityServiceRequest.java
+++ app/src/main/java/com/example/a2105projectgroup13/ActivityServiceRequest.java
@@ -1,5 +1,6 @@
 package com.example.a2105projectgroup13;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ActivityServiceRequest extends AppCompatActivity {
     //instance variables
@@ -16,6 +18,7 @@ public class ActivityServiceRequest extends AppCompatActivity {
     private FirebaseDatabase firebaseDatabase;
     private Button acceptButton, rejectButton, backButton;
     private ListView formFilledFieldsListView, attachmentsListView;
+    private String branchId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,18 +27,42 @@ public class ActivityServiceRequest extends AppCompatActivity {
 
         initializeInstanceVariables();
 
+        //The back button returns the user to the list of service requests received by the branch account
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO: move user to previous activity
+                startActivity(new Intent(ActivityServiceRequest.this, BranchViewServiceRequests.class));
+            }
+        });
+
+        //The accept action sets the status of the service request to "Accepted"
+        acceptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                acceptRequest();
+            }
+        });
+
+        //The reject action sets the status of the service request to "Rejected"
+        rejectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rejectRequest();
             }
         });
     }
 
+    /**
+     * Initializes all of the instance variables.
+     */
     private void initializeInstanceVariables() {
         //initialize firebase
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseDatabase = firebaseDatabase.getInstance();
+
+        //getting the branch id
+        FirebaseUser branchAccount = firebaseAuth.getInstance().getCurrentUser();
+        branchId = branchAccount.getUid();
 
         //initialize buttons
         acceptButton = (Button)findViewById(R.id.acceptButton);
@@ -45,5 +72,19 @@ public class ActivityServiceRequest extends AppCompatActivity {
         //initialize ListViews
         formFilledFieldsListView = (ListView)findViewById(R.id.formFilledFieldsListView);
         attachmentsListView = (ListView)findViewById(R.id.attachmentsListView);
+    }
+
+    /**
+     * Sets the service request status to "Accepted"
+     */
+    private void acceptRequest() {
+
+    }
+
+    /**
+     * Sets the service request status to "Rejected"
+     */
+    private void rejectRequest() {
+
     }
 }
